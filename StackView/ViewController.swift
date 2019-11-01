350e4f07ac0e465540abf8a47b0d221549f50d39
Harshit <harshit.2151.dev@gmail.com>
2019-11-02 00:14:19 +0530

    test

---
+++ StackView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Harshit Kumar on 20/10/19.
+//  Copyright © 2019 Harshit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+     @IBOutlet private weak var buton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    deinit {
+        print("ViewController deinit")
+    }
+}
+
